1c9dfae033da4030ee6c8715ec8d8687c38dc00c
gastonllaupe <llaupeg@gmail.com>
2026-04-12 22:13:42 -0300

    contador de generos

--- src/components/MediaList/MediaList.jsx
+++ src/components/MediaList/MediaList.jsx
@@ -5,13 +5,29 @@ function MediaList({item}) {
 
   const noVistas = item.filter(p => !p.visto);
   const vistas = item.filter(p => p.visto);
+  const contarGeneros = (array) => {
+  return array.reduce((acc, p) => {
+    acc[p.genero] = (acc[p.genero] || 0) + 1;
+    return acc;
+  }, {});
+  };
+  const generosNoVistas = contarGeneros(noVistas);
+  const generosVistas = contarGeneros(vistas);
 
   return (
     <div className={styles.display}>
       
       <div className={styles.porVer}>
         <div className={styles.datosPorVer}><h3>Por ver</h3>
-        <div className={styles.items}>{noVistas.length} items </div></div>
+          <div className={styles.items}>
+            {noVistas.length} {noVistas.length === 1 ? "item :" : "items :"}
+            {Object.entries(generosNoVistas).map(([genero, cantidad]) => (
+              <div key={genero}>
+                {genero} ({cantidad})
+              </div>
+            ))}
+  </div>
+        </div>
         {noVistas.length === 0? (
           <div className={styles.emptyPorVer}>
             No tenes contenido por ver.
@@ -33,7 +49,15 @@ function MediaList({item}) {
 
       <div className={styles.vistas}>
         <div className={styles.datosVista}><h3>Vistas</h3>
-        <div className={styles.items}>{vistas.length} items </div></div>
+          <div className={styles.items}>
+            {vistas.length} {vistas.length === 1 ? "item :" : "items :"}
+            {Object.entries(generosVistas).map(([genero, cantidad]) => (
+              <div key={genero}>
+                {genero} ({cantidad})
+              </div>
+            ))}
+          </div>
+        </div>
         {vistas.length===0? (
           <div className={styles.emptyVistas}>
             No tenes contenido visto.
